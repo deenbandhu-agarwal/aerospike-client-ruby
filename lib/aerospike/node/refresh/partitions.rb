b89426297a934b1c0733cdf8d1f03bb71a31704f
Sebastian Wallin <sebastian.wallin@gmail.com>
2018-03-12 20:16:18 -0700

    Fix refresh partitions and add specs

--- lib/aerospike/node/refresh/partitions.rb
+++ lib/aerospike/node/refresh/partitions.rb
@@ -5,12 +5,25 @@ module Aerospike
     module Refresh
       module Partitions
         class << self
-          def call(node, e)
+          def call(node, peers)
+            return unless should_refresh?(node, peers)
+
             conn = node.tend_connection
             node.cluster.update_partitions(conn, node)
           rescue ::Aerospike::Exceptions::Aerospike => e
             conn.close if conn
             Refresh::Failed.(node, e)
+          end
+
+          # Do not refresh partitions when node connection has already failed
+          # during this cluster tend iteration. Also, avoid "split cluster"
+          # case where this node thinks it's a 1-node cluster. Unchecked, such
+          # a node can dominate the partition map and cause all other nodes to
+          # be dropped.
+          def should_refresh?(node, peers)
+            return false if node.failed? || !node.active?
+            return false if !node.has_peers? && peers.refresh_count > 1
+            true
           end
         end
       end
--- spec/aerospike/node/refresh/partitions_spec.rb
+++ spec/aerospike/node/refresh/partitions_spec.rb
@@ -1,5 +1,50 @@
 # frozen_string_literal: true
 
 RSpec.describe Aerospike::Node::Refresh::Partitions do
-  pending
+  let(:node) { double }
+  let(:cluster) { double }
+  let(:conn) { double}
+  let(:peers) { double }
+  let(:refresh_count) { 10 }
+  let(:healthy) { true }
+
+  before do
+    allow(node).to receive(:tend_connection).and_return(conn)
+    allow(node).to receive(:cluster).and_return(cluster)
+    allow(cluster).to receive(:update_partitions)
+    allow(conn).to receive(:close)
+    allow(::Aerospike::Node::Refresh::Failed).to receive(:call)
+  end
+
+  describe '::call' do
+    subject(:refresh) { described_class.call(node, peers) }
+
+    before { allow(described_class).to receive(:should_refresh?).and_return(healthy) }
+
+    context 'with healty node' do
+      before { refresh }
+
+      it { expect(node).to have_received(:tend_connection) }
+      it { expect(cluster).to have_received(:update_partitions) }
+    end
+
+    context 'with failed node' do
+      let(:healthy) { false }
+
+      before { refresh }
+
+      it { expect(node).not_to have_received(:tend_connection) }
+    end
+
+    context 'when cluster.update_partitions fails' do
+      before do
+        allow(cluster).to receive(:update_partitions).and_raise(::Aerospike::Exceptions::Aerospike.new(0))
+
+        refresh
+      end
+
+      it { expect(conn).to have_received(:close) }
+      it { expect(::Aerospike::Node::Refresh::Failed).to have_received(:call) }
+    end
+  end
 end
